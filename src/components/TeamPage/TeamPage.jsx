43ebc14c4041c3baff4e0b8f7a413fbe113b139d
GOATAFI <projectexecutiveofpm@gmail.com>
2023-05-18 02:49:06 +0600

    individual team data show Done

--- src/components/TeamPage/TeamPage.jsx
+++ src/components/TeamPage/TeamPage.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 function TeamPage() {
   const { teamName } = useParams();
   const [teamData, setTeamData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(`http://localhost:5000/teams/${teamName}`)
@@ -11,23 +12,61 @@ function TeamPage() {
       .then(data => setTeamData(data));
   }, [teamName]);
 
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPlayers = teamData.filter(player => {
+    const searchRegex = new RegExp(searchTerm, 'i');
+    return (
+      searchRegex.test(player.PName) ||
+      searchRegex.test(player.Position) ||
+      searchRegex.test(player.Age) ||
+      searchRegex.test(player.Nationality) ||
+      searchRegex.test(player.Jersey_Num) ||
+      searchRegex.test(player.Salary) ||
+      searchRegex.test(player.Contract)
+    );
+  });
+
   return (
-    <div>
-      <h1>Team Page: {teamName}</h1>
-      <h2>Team Details:</h2>
-      <ul>
-        {teamData.map(player => (
-          <li key={player.PName}>
-            <strong>Name:</strong> {player.PName}<br />
-            <strong>Age:</strong> {player.Age}<br />
-            <strong>Nationality:</strong> {player.Nationality}<br />
-            <strong>Jersey Number:</strong> {player.Jersey_Num}<br />
-            <strong>Position:</strong> {player.Position}<br />
-            <strong>Salary:</strong> {player.Salary} million<br />
-            <strong>Contract:</strong> {player.Contract}
-          </li>
-        ))}
-      </ul>
+    <div className="overflow-x-auto mt-5 mb-9">
+      <h2 className="text-center text-4xl font-bold mb-8">Team Details</h2>
+      <div className="form-control w-96 mx-2 mb-7">
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <table className="table table-compact w-full">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Nationality</th>
+            <th>Jersey Number</th>
+            <th>Position</th>
+            <th>Salary</th>
+            <th>Contract</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredPlayers.map(player => (
+            <tr key={player.PName}>
+              <td>{player.PName}</td>
+              <td>{player.Age}</td>
+              <td>{player.Nationality}</td>
+              <td>{player.Jersey_Num}</td>
+              <td>{player.Position}</td>
+              <td>{player.Salary} Millions</td>
+              <td>{player.Contract}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
